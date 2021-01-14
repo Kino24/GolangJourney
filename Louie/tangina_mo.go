5cc3ea95a4659e43d053629453372fda5593eeab
Kino <joaquinsantos409@gmail.com>
2021-01-14 19:45:26 +0800

    Lou

---
+++ Louie/tangina_mo.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Sup, can a loc come up in your crib?")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	fmt.Println("Man fuck you, I'll see you at work.")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	fmt.Println("Ah nigga, don't hate me 'cause I'm beautiful, nigga.")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	fmt.Println("Maybe if you got rid of that ol' yee-yee ass haircut, you'd get some bitches on your dick.")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	fmt.Println("Nah, better yet, maybe Tanisha'd call your dog ass if she ever stop fucking with that\n brain surgeon or lawyer she fucking with.")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	fmt.Println("Niggaaaaaaaa...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	fmt.Println("What?!")
+}
